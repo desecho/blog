92f56b31c94f620c4e03a5a4f2606dbd21a40805
Anton Samarchyan <desecho@gmail.com>
2022-06-03 20:43:47 -0400

    Changes

--- scripts/new_post.sh
+++ scripts/new_post.sh
@@ -8,16 +8,4 @@ cd "$(dirname "$0")/.."
 post_name=$1
 post_file_name=$post_name.md
 hugo new "posts/$post_file_name"
-post_file_path="content/posts/$post_file_name"
 
-# Remove the last 2 lines of the post file
-# Value is 3 because it includes the last newline
-content=$(head -n -3 "$post_file_path")
-echo "$content" > "$post_file_path"
-
-cat >> "$post_file_path" <<- EOM
-comments: true
-categories: ['']
-keywords:
----
-EOM
